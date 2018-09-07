66909c23112e5dfae935bc33aac127a972f23c04
jeffer.ng <jeffer.ng@MU-C1-14.mullen.pvt.k12.co.us>
2018-09-07 15:02:55 -0600

    few more adjustments to last challenge

--- Digit Stripping/src/DigitStripping.java
+++ Digit Stripping/src/DigitStripping.java
@@ -3,10 +3,15 @@ public class DigitStripping
 	{
 
 		static Scanner userInput = new Scanner(System.in);
+		static int numbers;
+		static int digits;
+		static int[] userNumbers;
+		static int sum;
+		
 		public static void main(String[] args)
 			{
 				
-				fillArray();
+				//fillArray();
 				reportAmountOfDigits();
 				reportOddNumbers();
 				addingDigits();
@@ -51,31 +56,85 @@ public class DigitStripping
 			{
 				
 				System.out.println("Input numbers");
-				int numbers = userInput.nextInt();
+				numbers = userInput.nextInt();
+				int originalNumber = numbers;
 				
+				digits = 0;
 				
+				while(numbers != 0)
+				{		
+					numbers = numbers / 10;										
+					digits++;			
+				}
 				
+				numbers = originalNumber;
 				
+				System.out.println(digits);
 				
-				
-				
+				userNumbers = new int[digits];
 			}
 
 		private static void reportOddNumbers()
 			{
-				// TODO Auto-generated method stub
+				int oddNumbers = 0;
+				for(int i = 0; i < userNumbers.length; i++)
+					{
+						
+						userNumbers[i] = numbers % 10;
+						numbers = numbers / 10;
+						
+						if(userNumbers[i] % 2 == 1)
+							{
+								
+								oddNumbers++;
+								
+							}
+						
+						
+						
+					}
+				
+				System.out.println(oddNumbers);
+				
+				
+				
+				
+				
+				
+				
+				
+				
 				
 			}
 
 		private static void addingDigits()
 			{
-				// TODO Auto-generated method stub
+				
+				sum = 0;
+				
+				for(int i = 0; i < userNumbers.length; i++)
+					{
+						
+						sum += userNumbers[i];
+						
+						
+						
+					}
+				
+				
+				System.out.println(sum);
+				
 				
 			}
 
 		private static void roundingToNearestTen()
 			{
-				// TODO Auto-generated method stub
+				
+				sum = sum / 10;
+				
+				System.out.println(sum + "0");
+				
+				
 				
 			}
 
